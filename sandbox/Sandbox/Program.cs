c306ad8122f7cc154f5974d0809a3b09bc73559d
Allyrod <92774592+Allyrod@users.noreply.github.com>
2024-05-04 12:27:01 -0600

    changed code for prep pract

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,31 @@
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello me World!");
+//         Console.WriteLine("Hello my friend!");
+
+//         Console.WriteLine("What is your fav food?");
+//         string food = Console.ReadLine();
+//         Console.WriteLine($"Your fav food is {food}!");
+//     }
+// }
+
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello me World!");
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your lastname? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"your name is {last}, {first} {last})");
     }
 }

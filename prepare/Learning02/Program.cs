ff2c14aec60689192458b896683ecfd8e9c4891f
Allyrod <92774592+Allyrod@users.noreply.github.com>
2024-05-15 00:24:08 -0600

    Updated program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,26 +1,25 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
-
-
-
 class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         Job job1 = new Job();
+        
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2020;
+        job1._startYear = 2019;
         job1._endYear = 2022;
 
-        Job job2 = new Job();
+        Job job2 = new Job ();
+
         job2._jobTitle = "Manager";
         job2._company = "Apple";
-        job2._startYear = 2023;
-        job2._endYear = 2024;
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        
+       
+       job1. DisplayJobDetails();
+
+
     }
-
-    
 }
